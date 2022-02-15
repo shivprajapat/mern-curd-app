c1e8f5ae6bdb1baf5507b2582ac742daf382091d
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-15 16:25:34 +0530

    Home Table Added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header';
+import Home from './components/Home';
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Container, Table, Button } from 'react-bootstrap'
+import { FaEye, FaRegEdit, FaRegTrashAlt } from 'react-icons/fa';
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <Container>
+                <div className="add_btn mb-2">
+                    <button className='btn btn-primary'>Add Data</button>
+                </div>
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr className='table-dark'>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Job</th>
+                            <th colSpan={2}>Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td rowSpan={2}>1</td>
+                            <td>Shiv</td>
+                            <td>shiv@gmail.com</td>
+                            <td>software developer</td>
+                            <td>1234567890</td>
+                            <td className='d-flex justify-content-between'>
+                                <Button variant="success"><FaEye size={20} /></Button>
+                                <Button variant="primary"><FaRegEdit size={20} /></Button>
+                                <Button variant="warning"><FaRegTrashAlt size={20} /></Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+    )
+}
