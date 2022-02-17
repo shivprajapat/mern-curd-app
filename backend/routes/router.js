e91ca6fd476d1cdaa488df77b9efc541b08e7781
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-17 10:38:30 +0530

    node js post api static data added

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,12 +1,35 @@
 const express = require('express') //import express
 const router = express.Router() //create express router
+const users = require('../modules/userSchema') //import userSchema
 
 // router.get('/', (req, res) => {
 //     res.send('connect')
 // })
 
-router.post("/register", (req, res) => {
-    console.log(req.body);
+router.post("/register", async (req, res) => {
+
+    const { name, email, age, mobile, work, add, desc } = req.body;
+    if (!name || !email || !age || !mobile || !work || !add || !desc) {
+        res.status(404).json('please fill all the fields');
+    }
+    try {
+        const preuser = await users.findOne({ email: email });
+        console.log(preuser);
+
+        if (preuser) {
+            res.status(404).json('users already exist');
+        } else {
+            const adduser = new users({
+                name, email, age, mobile, work, add, desc
+            });
+            await adduser.save();
+            res.status(201).json(adduser);
+            console.log(adduser);
+        }
+
+    } catch (error) {
+        res.status(404).json(error);
+    }
 })
 
 module.exports = router;
